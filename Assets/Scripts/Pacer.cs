4fc404e82c349fc0fc17cfac5b5f4d3b22deeb5c
twians <35275389+twians@users.noreply.github.com>
2018-03-20 16:08:50 -0500

    RollABall Update #1

---
+++ Assets/Scripts/Pacer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pacer : MonoBehaviour {
+
+    public float speed = 0.5f;
+
+    private float zMax = 7.5f;
+    private float zMin = -7.5f;
+    private int direction = 1;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        float zNew = transform.position.z + direction * speed * Time.deltaTime;
+
+        if (zNew >= zMax)
+        {
+            zNew = zMax;
+            direction *= -1;
+        }
+        else if (zNew <= zMin)
+        {
+            zNew = zMin;
+            direction *= -1;
+        }
+
+        transform.position = new Vector3(7.5f, 0.75f, zNew);
+	}
+}
